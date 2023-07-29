f1716f9c88ece919bed595eba00fdaa37bd08a6e
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-07-30 01:52:39 +0300

    Dog class and method added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(Dog.shouldWakeUp(true, 1));
+        System.out.println(Dog.shouldWakeUp(false, 2));
+        System.out.println(Dog.shouldWakeUp(true, 8));
+        System.out.println(Dog.shouldWakeUp(true, -1));
     }
+
+
 }
